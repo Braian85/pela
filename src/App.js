efc4c1ed9647ce45dac1dc32f5be55d765f5bac7
Braian Savoretti <braiansavoretti@192.168.1.3>
2022-03-26 21:35:31 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Swict, route  } from 'react-router-dom';
+import NavBar from "./Components/Navegacion/NavBar";
+import Inicio from './Components/Paginas/Inicio';
+import Coach from './Components/Paginas/Coach';
+import Items from './Components/Paginas/Items';
+
+ function App() {
+
+  return (
+    <div className="App">  
+    <>
+    <Router>
+      <NavBar/>
+      <switch>
+        <route path='/' exact component={Inicio}/>
+        <route path='/Coach' exact component={Coach}/>
+        <route path='/Items' exact component={Items}/>
+      </switch>
+    </Router>
+    </>
+    </div>
+    )
+  };
+
+
+export default App;
